49fe55029bedcb9d22c91d6ed6af977b1d228f96
Nawawish <hydrokidz@gmail.com>
2018-09-12 17:24:58 +0700

    Fix fizzBuzz and isPrime function

--- src/utils/exercise.js
+++ src/utils/exercise.js
@@ -4,28 +4,33 @@ const sum = (...args) =>
 const insertData = (storage, data) => [...storage, ...[data]];
 
 const fizzBuzz = input => {
-  let result;
-
-  switch (input) {
-    case input % 3 === 0:
-      result = "Fizz";
-      break;
-    case input % 5 === 0:
-      result = "Buzz";
-      break;
-    case input % 15 === 0:
-      result = "FizzBuzz";
-      break;
-
-    default:
-      result = input;
-      break;
+  if (input % 15 === 0) {
+    return "FizzBuzz";
   }
 
-  return result;
+  if (input % 5 === 0) {
+    return "Buzz";
+  }
+
+  if (input % 3 === 0) {
+    return "Fizz";
+  }
+
+  return input;
 };
 
-const isPrime = input => input % 2;
+// I did not create this function by myself.
+// Maybe due to lack of appropriate knowledge in Mathematic.
+// I copy it from: https://stackoverflow.com/questions/40200089/is-a-number-prime
+const isPrime = input => {
+  for (let i = 2; i < input; i++) {
+    if (input % i === 0) {
+      return false;
+    }
+  }
+
+  return input !== 1;
+};
 
 const calculator = (a, b) => ({
   add: () => a + b,
@@ -34,4 +39,4 @@ const calculator = (a, b) => ({
   multi: () => a * b
 });
 
-export default { sum, insertData, fizzBuzz, isPrime, calculator };
+module.exports = { sum, insertData, fizzBuzz, isPrime, calculator };
